a42a4737a9e3038530919c976e8db42752170e25
Stefano Speretta <s.speretta@tudelft.nl>
2019-08-26 09:57:02 +0200

    Added MCU initialisation function
    Initiaises the MCU high-speed clock, common to all boards

---
+++ DelfiPQcore.cpp
@@ -0,0 +1,29 @@
+/*
+ * DelfiPQcore.cpp
+ *
+ *  Created on: 26 Aug 2019
+ *      Author: stefanosperett
+ */
+
+#include <DelfiPQcore.h>
+
+void DelfiPQcore::initMCU()
+{
+    // Configuring pins for HF XTAL
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_PJ,
+            GPIO_PIN3 | GPIO_PIN2, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    // Starting HFXT in non-bypass mode without a timeout. Before we start
+    // we have to change VCORE to 1 to support the 48MHz frequency
+    MAP_CS_setExternalClockSourceFrequency(0, FCLOCK);
+    MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
+    MAP_FlashCtl_setWaitState(FLASH_BANK0, 2);
+    MAP_FlashCtl_setWaitState(FLASH_BANK1, 2);
+    MAP_CS_startHFXT(false);
+
+    // Configure clocks that we need
+    MAP_CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    MAP_CS_initClockSignal(CS_SMCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_4);
+    MAP_CS_initClockSignal(CS_HSMCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_2);
+
+}
---
+++ DelfiPQcore.h
@@ -0,0 +1,22 @@
+/*
+ * DelfiPQcore.h
+ *
+ *  Created on: 26 Aug 2019
+ *      Author: stefanosperett
+ */
+
+#ifndef DELFIPQCORE_H_
+#define DELFIPQCORE_H_
+
+#include <driverlib.h>
+#include "msp.h"
+
+#define FCLOCK 48000000
+
+class DelfiPQcore
+{
+public:
+    static void initMCU();
+};
+
+#endif /* DELFIPQCORE_H_ */
