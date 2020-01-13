948cb6a3094a15b3345aa86c5dee28af67184979
CasperBroekhuizen <broekhuizen.casper@gmail.com>
2020-01-13 11:08:13 +0100

    Init commit of updateService

---
+++ SoftwareVersionService.h
@@ -0,0 +1,37 @@
+/*
+ * SoftwareVersionService.h
+ *
+ *  Created on: 02 Dec 2019
+ *      Author: Casper Broekhuizen
+ */
+
+#ifndef SOFTWAREVERSIONSERVICE_H_
+#define SOFTWAREVERSIONSERVICE_H_
+
+#include "PQ9Bus.h"
+#include "PQ9Frame.h"
+#include "Service.h"
+#include "DSerial.h"
+
+#define xstr(s) str(s)
+#define str(s) #s
+
+#define SOFTWAREVERSION_SERVICE         23
+
+#define SOFTWAREVERSION_GETSOFTWAREVERSION       1
+#define SOFTWAREVERSION_ACCEPT          2
+#define SOFTWAREVERSION_ERROR           0
+
+class SoftwareVersionService: public Service
+{
+ public:
+    SoftwareVersionService();
+    SoftwareVersionService(uint8_t versionNumberIn[]);
+    virtual bool process( PQ9Frame &command, PQ9Sender &interface, PQ9Frame &workingBbuffer );
+
+private:
+    bool hasVersionNumber;
+    uint8_t versionNumber[8] = {0};
+};
+
+#endif /* SOFTWAREVERSIONSERVICE_H_ */
