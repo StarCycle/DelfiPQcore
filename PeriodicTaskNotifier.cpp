e352f39656140fab495fbd4a70bca4afcf1d6bd8
CasperBroekuizen <broekhuizen.casper@gmail.com>
2020-01-23 09:14:31 +0100

    Temporary Fix of TaskNotifier being too fast.
    relates to issue https://github.com/DelfiSpace/EPS_FlightSoftware/issues/9

--- PeriodicTaskNotifier.cpp
+++ PeriodicTaskNotifier.cpp
@@ -10,16 +10,25 @@
 PeriodicTaskNotifier *notifierStub;
 
 void NotifyTasks_stub(){
+    //Temporary Fix for WDT Kicking too fast
+    MAP_Timer32_clearInterruptFlag(TIMER32_0_BASE);
     notifierStub->NotifyTasks();
 };
 
 PeriodicTaskNotifier::PeriodicTaskNotifier(int count, Task** taskListIn, int nrOfTasks ) :
     taskList(taskListIn), numberOfTasks(nrOfTasks){
     notifierStub = this;
-    MAP_SysTick_enableModule();
-    MAP_SysTick_setPeriod(count);
-    MAP_SysTick_registerInterrupt(&NotifyTasks_stub);
-    MAP_SysTick_enableInterrupt();
+//    MAP_SysTick_enableModule();
+//    MAP_SysTick_setPeriod(count);  << TODO: count is only 24-bits
+//    MAP_SysTick_registerInterrupt(&NotifyTasks_stub);
+//    MAP_SysTick_enableInterrupt();
+
+    //Temporary Fix for WDT Kicking too fast
+    MAP_Timer32_initModule(TIMER32_0_BASE, TIMER32_PRESCALER_1, TIMER32_32BIT,
+                TIMER32_PERIODIC_MODE);
+    MAP_Timer32_registerInterrupt(TIMER32_0_INTERRUPT, &NotifyTasks_stub);
+    MAP_Timer32_setCount(TIMER32_0_BASE, count);
+    MAP_Timer32_startTimer(TIMER32_0_BASE, false);
 };
 
 
