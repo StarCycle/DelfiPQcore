7c63b7b183d954fc6e281b130b70096d64064596
Casper <broekhuizen.casper@gmail.com>
2020-07-23 12:35:44 +0200

    added empty constructor and selection of auto-persistancy in constructor

--- FRAMBackedVar.h
+++ FRAMBackedVar.h
@@ -17,32 +17,65 @@ class FRAMBackedVar{
 
 private:
     varType RAMcopy;
+    bool autoUpdate;
     unsigned long FRAMAddress;
     MB85RS* FRAM;
 
 public:
+
+    FRAMBackedVar(){
+        FRAM = 0;
+        FRAMAddress = 0;
+        autoUpdate = false;
+    }
+
     FRAMBackedVar(MB85RS& fram, unsigned long address){
         FRAM = &fram;
         FRAMAddress = address;
+        autoUpdate = false;
     }
 
-    void init(bool updateFromFram){
-        if(updateFromFram){
-            FRAM->read(FRAMAddress, (unsigned char*)&RAMcopy, sizeof(RAMcopy));
-        }else{
-            RAMcopy = 0;
-            FRAM->write(FRAMAddress, (unsigned char*)&RAMcopy, sizeof(RAMcopy));
+    void init(MB85RS& fram, unsigned long address, bool initFromFRAM, bool autoPersistency){
+        FRAM = &fram;
+        FRAMAddress = address;
+        autoUpdate = autoPersistency;
+
+        if(FRAM){
+            if(initFromFRAM){
+                FRAM->read(FRAMAddress, (unsigned char*)&RAMcopy, sizeof(RAMcopy));
+            }else{
+                RAMcopy = 0;
+                FRAM->write(FRAMAddress, (unsigned char*)&RAMcopy, sizeof(RAMcopy));
+            }
         }
     }
 
+    void init(bool initFromFRAM, bool autoPersistency){
+        autoUpdate = autoPersistency;
+
+        if(FRAM){
+            if(initFromFRAM){
+                FRAM->read(FRAMAddress, (unsigned char*)&RAMcopy, sizeof(RAMcopy));
+            }else{
+                RAMcopy = 0;
+                FRAM->write(FRAMAddress, (unsigned char*)&RAMcopy, sizeof(RAMcopy));
+            }
+        }
+    }
 
     varType read(){
         return RAMcopy;
     }
 
+    void save(){
+        FRAM->write(FRAMAddress, (unsigned char*)&RAMcopy, sizeof(RAMcopy));
+    }
+
     void write(varType value){
         RAMcopy = value;
-        FRAM->write(FRAMAddress, (unsigned char*)&RAMcopy, sizeof(RAMcopy));
+        if(autoUpdate){
+            save();
+        }
     }
 
     int getSize(){
@@ -50,22 +83,32 @@ public:
     }
 
     // Overloading of Assignment Operator
-    varType operator=(varType newValue) {
+    void operator=(varType newValue) {
         write(newValue);
-    }
+    };
+    void operator+=(varType addValue) {
+        write(RAMcopy+addValue);
+    };
+    void operator-=(varType minValue) {
+        write(RAMcopy-minValue);
+    };
+
 
     // Overload typecasting to varType;
     operator varType() const { return RAMcopy; }
+    // Overload typecasting to varType*;
+    operator varType*() const { return &RAMcopy; }
+
 
     // Overload Addition
     varType operator+(varType value) {
          return RAMcopy + value;
-    }
+    };
 
     // Overload Subtraction
     varType operator-(varType value) {
          return RAMcopy - value;
-    }
+    };
 
 };
 
